566650e7b54419d108bb66d6332ee71412a5a16a
Abdullah <abdullahkocak33@hotmail.com>
2025-06-18 18:41:29 +0300

    Starting project

---
+++ src/main/java/dev/patika/veterinary/entities/Customer.java
@@ -0,0 +1,36 @@
+package dev.patika.veterinary.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "customers")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "customer_id")
+    private Integer id;
+
+    @NotNull
+    @Column(name = "customer_name")
+    private String name;
+
+    @Column(name = "customer_phone")
+    private String phone;
+
+    @Column(name = "customer_mail")
+    private String mail;
+
+    @Column(name = "customer_address")
+    private String address;
+
+    @Column(name = "customer_city")
+    private String city;
+}
